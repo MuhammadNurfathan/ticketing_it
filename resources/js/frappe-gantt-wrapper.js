2485aa1a1e488d457e9f4e767dc308a16b22efea
MuhammadNurfathan <muhammadnurfathan07@gmail.com>
2025-11-05 17:01:10 +0700

    comiittt package offline

---
+++ resources/js/frappe-gantt-wrapper.js
@@ -0,0 +1,7 @@
+// frappe-gantt-wrapper.js
+// Import script asli
+import './frappe-gantt.min.js';
+
+// Frappe Gantt biasanya auto-assign ke window.Gantt
+// Export ulang sebagai ES module
+export default window.Gantt;
