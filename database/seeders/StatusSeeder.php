6a90315379666a84f49de2bfa589159a77334854
MuhammadNurfathan <muhammadnurfathan07@gmail.com>
2026-04-01 16:04:03 +0700

    benerin seeder

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -15,38 +15,47 @@ class StatusSeeder extends Seeder
         Status::insert([
             [
                 'name' => 'Waiting',
+                'type' => 'waiting',
                 'context' => 'ticket',
             ],
             [
                 'name' => 'In Progress',
+                'type' => 'in_progress',
                 'context' => 'ticket',
             ],
             [
                 'name' => 'Done',
+                'type' => 'done',
                 'context' => 'ticket',
             ],
             [
                 'name' => 'Void',
+                'type' => 'void',
                 'context' => 'ticket',
             ],
             [
                 'name' => 'Feedback',
+                'type' => 'feedback',
                 'context' => 'ticket',
             ],
             [
                 'name' => 'Waiting',
+                'type' => 'waiting',
                 'context' => 'project',
             ],
             [
                 'name' => 'In Progress',
+                'type' => 'in_progress',
                 'context' => 'project',
             ],
             [
                 'name' => 'Resolved',
+                'type' => 'resolved',
                 'context' => 'project',
             ],
             [
                 'name' => 'Pending',
+                'type' => 'pending',
                 'context' => 'project',
             ],
         ]);
